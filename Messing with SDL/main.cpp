c73f5ce1a8e847e3b94cda397ee74f4b90b327d8
Nerdwerd <50635875+Nerdwerd@users.noreply.github.com>
2020-02-21 22:37:56 -0600

    Fixed SDL System

--- Messing with SDL/main.cpp
+++ Messing with SDL/main.cpp
@@ -1,7 +1,12 @@
 #include <iostream>
+#include <SDL.h>
 
 using namespace std;
 
-int main() {
+int main(int argc, char *argv[]) {
+	if (SDL_Init(SDL_INIT_EVERYTHING) < 0) {
+		cout << "SDL could not initialize! SDL Error: " << SDL_GetError() << endl;
+	}
 
+	return EXIT_SUCCESS;
 }
