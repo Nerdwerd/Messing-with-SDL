888a1e367bfe6e7ab688c2b68dd7398250698ce4
Nerdwerd <50635875+Nerdwerd@users.noreply.github.com>
2020-02-21 22:44:45 -0600

    Window Setup

--- Messing with SDL/main.cpp
+++ Messing with SDL/main.cpp
@@ -3,10 +3,31 @@
 
 using namespace std;
 
+const int WIDTH = 800, HEIGHT = 600;
+
 int main(int argc, char *argv[]) {
 	if (SDL_Init(SDL_INIT_EVERYTHING) < 0) {
 		cout << "SDL could not initialize! SDL Error: " << SDL_GetError() << endl;
 	}
+
+	SDL_Window* window = SDL_CreateWindow("Hello World", SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, WIDTH, HEIGHT, SDL_WINDOW_ALLOW_HIGHDPI);
+
+	if (NULL == window) {
+		cout << "Could not create window: " << SDL_GetError << endl;
+	}
+
+	SDL_Event windowEvent;
+
+	while (true) {
+		if (SDL_PollEvent(&windowEvent)) {
+			if (SDL_QUIT == windowEvent.type) {
+				break;
+			}
+		}
+	}
+
+	SDL_DestroyWindow(window);
+	SDL_Quit();
 
 	return EXIT_SUCCESS;
 }
